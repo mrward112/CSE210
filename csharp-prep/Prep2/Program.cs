eacf0c3f5b5755510371fe3f0797c336a177ff08
Merrick Ward <war19027@byui.edu>
2023-04-24 13:08:00 -0600

    finished prep 2 requirments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your current grade percentage? ");
+        string userInputGrade = Console.ReadLine();
+        int Grade = int.Parse(userInputGrade);
+
+    //Test for letter grade
+
+        if (Grade >= 90)
+        {
+            Console.WriteLine("your grade is an A. Good job!");
+        }
+        else if (Grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (Grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (Grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is an F. Try retaking the class");
+        }
+
+
+    //Test for passing grade
+        if (Grade >= 70)
+        {
+            Console.WriteLine("You have passed the class! ");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass the class. ");
+        }
+
     }
 }
